74f439a744767ec0ba3f19d7d4d63fa4a694f62a
Vinamra3215 <vinamragupta100@gmail.com>
2026-06-07 02:47:09 +0530

    update init files

--- src/xaura/__init__.py
+++ src/xaura/__init__.py
@@ -2,5 +2,6 @@
 
 __version__ = "0.1.0"
 
-# These will be uncommented as modules are built:
-# Day 3: from xaura.profiler import profile, DataProfile
+from xaura.profiler import profile, DataProfile
+
+__all__ = ["profile", "DataProfile", "__version__"]
