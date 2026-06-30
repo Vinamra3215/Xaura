2aa5777b18374ce7ca5418fb5378c6bfdf892b61
pmakde <181649771+pmakde@users.noreply.github.com>
2026-07-01 01:03:16 +0530

    adding clusters.py

--- src/xaura/models/clusterers/__init__.py
+++ src/xaura/models/clusterers/__init__.py
@@ -4,6 +4,8 @@ Importing this module triggers @register_model decorators,
 adding all clusterers to the global model registry.
 """
 
+from xaura.models.clusterers.dbscan import DBSCANModel
+from xaura.models.clusterers.hierarchical import HierarchicalModel
 from xaura.models.clusterers.kmeans import KMeansModel
 
-__all__ = ["KMeansModel"]
+__all__ = ["DBSCANModel", "HierarchicalModel", "KMeansModel"]
